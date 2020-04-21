8344ac192666a1b7b8d5d2d78f7f9a5a5f647d17
Hoang Nhan Duyen Nguyen <armyduyen55555@gmail.com>
2020-04-21 18:39:02 -0400

    About Chef, Open Hours section

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,10 +1,15 @@
 let navbar = $(".navbar");
 $(window).scroll(function () {
   // get the complete hight of window
-  let oTop = $(".section-2").offset().top - window.innerHeight;
+  let oTop = $(".section-1").offset().top - window.innerHeight;
   if ($(window).scrollTop() > oTop) {
     navbar.addClass("sticky");
   } else {
     navbar.removeClass("sticky");
   }
+});
+
+// To hide the collapse navbar after a link is clicked
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
 });
